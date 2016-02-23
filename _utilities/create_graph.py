12e4a6b9999b2861af7da820544535536b05b2be
cirnelle <yiling.hwong@gmail.com>
2016-02-23 11:09:39 +1100

    create graph for keywords

--- _utilities/create_graph.py
+++ _utilities/create_graph.py
@@ -4,55 +4,138 @@ import sys
 import os
 import networkx as nx
 
-G = nx.Graph()
+class CreateGraph():
 
-lines = open('../output/models/space_tweets_composition.txt','r').readlines()
-#lines = open('test.txt','r').readlines()
+    def create_topic_composition_graph(self):
 
-###################
-# Create edges list
-###################
+        G = nx.Graph()
 
-edges = []
+        lines = open(path_to_topic_composition_file,'r').readlines()
+        #lines = open('test.txt','r').readlines()
 
-for line in lines:
-    spline = line.replace('\n','').split('\t')
+        ###################
+        # Create edges list
+        ###################
 
-    for x in range(1,11):
+        edges = []
 
-        user = spline[1]
-        topic = 'Topic_'+str(spline[2*x])
-        weight = round((float(spline[(2*x)+1]) * 100),2)
+        number_of_topics = 10
+        n = number_of_topics + 1
 
-        edges.append([user,topic,{'weight':weight}])
+        for line in lines:
+            spline = line.replace('\n','').split('\t')
 
-print ("Number of edges is "+str(len(edges)))
+            for x in range(1,n):
 
-##################
-# Create node list
-##################
+                user = spline[1]
+                topic = 'Topic_'+str(spline[2*x])
+                weight = round((float(spline[(2*x)+1]) * 100),2)
 
-nodes = []
+                edges.append([user,topic,{'weight':weight}])
 
-# create nodes for each user
+        print ("Number of edges is "+str(len(edges)))
 
-for line in lines:
-    spline = line.replace('\n','').split('\t')
+        ##################
+        # Create node list
+        ##################
 
-    nodes.append(spline[1])
+        nodes = []
 
-# create nodes for each topic
+        # create nodes for each user
 
-for x in range(10):
-    nodes.append('Topic_'+str(x))
+        for line in lines:
+            spline = line.replace('\n','').split('\t')
 
-print ("Number of nodes is "+str(len(nodes)))
+            nodes.append(spline[1])
+
+        # create nodes for each topic
+
+        for x in range(number_of_topics):
+            nodes.append('Topic_'+str(x))
+
+        print ("Number of nodes is "+str(len(nodes)))
 
 
-G.add_edges_from(edges)
-G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+        G.add_nodes_from(nodes)
 
-nx.write_gexf(G, "../GEPHI/graph_files/space_tweets_raw.gexf")
+        nx.write_gexf(G, path_to_store_topic_composition_graph)
+
+
+    def create_topic_keywords_graph(self):
+
+        G = nx.Graph()
+
+        lines = open(path_to_topic_keywords_file,'r').readlines()
+
+        ##################
+        # create edge list
+        ##################
+
+        edges = []
+        number_of_keywords = 19
+        n = number_of_keywords + 2
+
+
+        for line in lines:
+            spline = line.replace('\n','').split('\t')
+
+            for x in range(2,n):
+                topic = 'Topic_'+str(spline[0])
+                keyword = spline[x]
+
+                edges.append([topic,keyword])
+
+        print ("Number of edges is "+str(len(edges)))
+
+        ##################
+        # create node list
+        ##################
+
+        nodes = []
+        keywords = []
+
+        for line in lines:
+            spline = line.replace('\n','').split('\t')
+            topic = 'Topic_'+str(spline[0])
+
+            nodes.append(topic)
+
+            for x in range(2,n):
+                if spline[x] not in keywords:
+                    keywords.append(spline[x])
+
+                    nodes.append(spline[x])
+
+                else:
+                    print ("Duplicated keyword "+str(spline[x]))
+
+        print ("Number of nodes is "+str(len(nodes)))
+
+        G.add_edges_from(edges)
+        G.add_nodes_from(nodes)
+
+        nx.write_gexf(G, path_to_store_topic_keywords_graph)
+
+
+###############
+# variables
+###############
+
+path_to_topic_composition_file = '../output/models/space_tweets_composition.txt'
+path_to_store_topic_composition_graph = "../GEPHI/graph_files/space_tweets_composition_raw.gexf"
+path_to_topic_keywords_file = '../output/models/space_tweets_keys.txt'
+path_to_store_topic_keywords_graph = '../GEPHI/graph_files/space_tweets_keys_raw.gexf'
+
+
+if __name__ == '__main__':
+
+    cg = CreateGraph()
+
+    cg.create_topic_composition_graph()
+    #cg.create_topic_keywords_graph()
+
+
 
 
 '''
